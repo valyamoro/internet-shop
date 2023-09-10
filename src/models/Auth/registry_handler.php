ddcfdbba652898d0854fb5c3e1a09fcf166a9ecb
kutlumbek <Moralek228@gmail.com>
2023-09-10 19:48:39 +0300

    Изменил комментарии, убрал проверку на наличие

--- src/models/Auth/registry_handler.php
+++ src/models/Auth/registry_handler.php
@@ -3,69 +3,65 @@ declare(strict_types=1);
 error_reporting(-1);
 session_start();
 
-if (!empty($_POST)) {
+$msg = false;
 
-    $msg = false;
-    $user = [];
+extract($_POST);
+extract($_FILES);
 
-    extract($_POST);
-    extract($_FILES);
-
-    // Валидация пришедших данных из $_POST и $_FILES.
-    if (empty($user_name)) {
-        $msg .= 'Заполните поле имя' . PHP_EOL;
-    } elseif (preg_match('#[^а-яa-z]#ui', $user_name)) {
-        $msg .= 'Имя содержит недопустимые символы' . PHP_EOL;
-    } elseif (mb_strlen($user_name) > 15) {
-        $msg .= 'Имя содержит больше 15 символов' . $user_name . PHP_EOL;
-    } elseif (mb_strlen($user_name) <= 3) {
-        $msg .= 'Имя содержит менее 4 символов' . $user_name . PHP_EOL;
-    }
-
-    if (empty($email)) {
-        $msg .= 'Заполните поле почты' . PHP_EOL;
-    } elseif (!preg_match("/[0-9a-z]+@[a-z]/", $email)) {
-        $msg .= 'Почта содержит недопустимые данные' . PHP_EOL;
-    }
-
-    if (empty($password)) {
-        $msg .= 'Заполните поле пароль' . PHP_EOL;
-    } elseif (!preg_match('/^(?![0-9]+$).+/', $password)) {
-        $msg .= 'Пароль не должен содержать только цифры' . PHP_EOL;
-    } elseif (!preg_match('/^[^!№;]+$/u', $password)) {
-        $msg .= 'Пароль содержит недопустимые символы' . PHP_EOL;
-    } elseif (!preg_match('/^(?![A-Za-z]+$).+/', $password)) {
-        $msg .= 'Пароль не должен состоять только из букв' . PHP_EOL;
-    } elseif (!preg_match('/[A-Z]/', $password)) {
-        $msg .= 'Пароль должен содержать минимум одну заглавную букву' . PHP_EOL;
-    } elseif (strlen($password) <= 5) {
-        $msg .= 'Пароль содержит меньше 5 символов ' . PHP_EOL;
-    } elseif (strlen($password) > 15) {
-        $msg .= 'Пароль больше 15 символов ' . PHP_EOL;
-    }
-
-    if (empty($phone_number)) {
-        $msg .= 'Заполните поле номер' . PHP_EOL;
-    } elseif (!preg_match('/((8|\+7)-?)?\(?\d{3,5}\)?-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}((-?\d{1})?-?\d{1})?/',
-        $phone_number)) {
-        $msg .= 'Некоректный номер' . $phone_number . PHP_EOL;
-    }
-
-    // Инициализация одобреднных данных файла.
-    $maxFileSize = 1 * 1024 * 1024;
-    $allowedExtensions = ['jpeg', 'png', 'gif', 'webp', 'jpg'];
-    // Получаю расширение пришедшего из $_FILES файла.
-    $extension = pathinfo($avatar['name'], PATHINFO_EXTENSION);
-
-    if (empty($avatar['name'])) {
-        $msg .= 'Аватар обязателен.';
-    } elseif (!in_array($extension, $allowedExtensions)) {
-        $msg .= 'Недопустимый тип файла.';
-    } elseif ($avatar['size'] > $maxFileSize) {
-        $msg .= 'Размер файла превышает допустимый.';
-    }
-
+// Валидация пришедших данных из $_POST и $_FILES.
+if (empty($user_name)) {
+    $msg .= 'Заполните поле имя' . PHP_EOL;
+} elseif (preg_match('#[^а-яa-z]#ui', $user_name)) {
+    $msg .= 'Имя содержит недопустимые символы' . PHP_EOL;
+} elseif (mb_strlen($user_name) > 15) {
+    $msg .= 'Имя содержит больше 15 символов' . $user_name . PHP_EOL;
+} elseif (mb_strlen($user_name) <= 3) {
+    $msg .= 'Имя содержит менее 4 символов' . $user_name . PHP_EOL;
 }
+
+if (empty($email)) {
+    $msg .= 'Заполните поле почты' . PHP_EOL;
+} elseif (!preg_match("/[0-9a-z]+@[a-z]/", $email)) {
+    $msg .= 'Почта содержит недопустимые данные' . PHP_EOL;
+}
+
+if (empty($password)) {
+    $msg .= 'Заполните поле пароль' . PHP_EOL;
+} elseif (!preg_match('/^(?![0-9]+$).+/', $password)) {
+    $msg .= 'Пароль не должен содержать только цифры' . PHP_EOL;
+} elseif (!preg_match('/^[^!№;]+$/u', $password)) {
+    $msg .= 'Пароль содержит недопустимые символы' . PHP_EOL;
+} elseif (!preg_match('/^(?![A-Za-z]+$).+/', $password)) {
+    $msg .= 'Пароль не должен состоять только из букв' . PHP_EOL;
+} elseif (!preg_match('/[A-Z]/', $password)) {
+    $msg .= 'Пароль должен содержать минимум одну заглавную букву' . PHP_EOL;
+} elseif (strlen($password) <= 5) {
+    $msg .= 'Пароль содержит меньше 5 символов ' . PHP_EOL;
+} elseif (strlen($password) > 15) {
+    $msg .= 'Пароль больше 15 символов ' . PHP_EOL;
+}
+
+if (empty($phone_number)) {
+    $msg .= 'Заполните поле номер' . PHP_EOL;
+} elseif (!preg_match('/((8|\+7)-?)?\(?\d{3,5}\)?-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}((-?\d{1})?-?\d{1})?/',
+    $phone_number)) {
+    $msg .= 'Некоректный номер' . $phone_number . PHP_EOL;
+}
+
+// Инициализация одобреднных данных файла.
+$maxFileSize = 1 * 1024 * 1024;
+$allowedExtensions = ['jpeg', 'png', 'gif', 'webp', 'jpg'];
+// Получаю расширение пришедшего из $_FILES файла.
+$extension = pathinfo($avatar['name'], PATHINFO_EXTENSION);
+
+if (empty($avatar['name'])) {
+    $msg .= 'Аватар обязателен.';
+} elseif (!in_array($extension, $allowedExtensions)) {
+    $msg .= 'Недопустимый тип файла.';
+} elseif ($avatar['size'] > $maxFileSize) {
+    $msg .= 'Размер файла превышает допустимый.';
+}
+
 
 if (!empty($msg)) {
     $_SESSION['msg'] = $msg;
@@ -104,7 +100,7 @@ if (!empty($msg)) {
     // Инициализации информации о пришедшем файле. Временный путь до файла и название файла.
     $tmpName = $avatar['tmp_name'];
 
-    $avatarName = $avatar['name'];
+    $avatarName = $avatar['name']; // зачем нужно создавать переменную? *
 
     // Путь до места хранения аватара пользователя.
     $filePath = $pathDirectoryUploadAvatar . uniqid() . $avatar['name'];
@@ -120,7 +116,7 @@ if (!empty($msg)) {
     // Создаем идентификатор новому пользователю.
     $userId = $dataUsers ? (intval(explode('|', end($dataUsers))[0]) + 1) : 1; // вернул обратно end *
 
-    // Приходящие данные из $_POST.
+    // Приходящие данные из $_POST. // удалить? *
 
     // Проверяем есть ли аккаунт с такой же почтой и телефоном в user.txt.
     $isUserExists = false;
