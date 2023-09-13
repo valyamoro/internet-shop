04bd2985055e4bf7df41aa19ecc1d815f5512b54
kutlumbek <Moralek228@gmail.com>
2023-09-14 00:04:53 +0300

    Получаю айди заказов авторизированного пользователя

--- src/models/Auth/orders/order_person.php
+++ src/models/Auth/orders/order_person.php
@@ -1,12 +1,62 @@
 <?php
-// Все заказы
-$filePath = __DIR__ . '/../../../../storage_files/order_product.txt';
+session_start();
 
-$ordersData = file($filePath, FILE_IGNORE_NEW_LINES);
+$orderPath = __DIR__ . '/../../../../storage_files/order.txt';
 
-foreach ($ordersData as $item) {
-    $item = explode('|', $item);
-    $orderId = $item[0];
+$userOrders = [];
+
+// Получаем информацию о заказах пользователя.
+$fileOrder = fopen($orderPath, 'r');
+if ($fileOrder) {
+    while (($line = fgets($fileOrder)) !== false) {
+        $line = trim($line);
+
+        $parts = explode('|', $line);
+
+        $userId = $parts[0];
+        $orderId = $parts[1];
+
+        if (!isset($userOrders[$userId])) {
+            $userOrders[$userId] = [];
+        }
+        $userOrders[$userId][] = $orderId;
+    }
+    fclose($fileOrder);
+
+} else {
+    echo 'Не удалось открыть файл';
 }
-print_r($orderId);
-die;
+
+//print_r($userOrders);
+
+$orderProductPath = __DIR__ . '/../../../../storage_files/order_product.txt';
+
+$orderProducts = [];
+
+$fileOrderProduct = fopen($orderProductPath, 'r');
+
+if ($fileOrderProduct) {
+    while (($line = fgets($fileOrderProduct)) !== false) {
+        $line = trim($line);
+
+        $parts = explode('|', $line);
+
+        $userId = $parts[1];
+        $orderId = $parts[0];
+        $productId = $parts[2];
+        $count = $parts[3];
+
+        print_r($orderProducts);
+        if (isset($orderProducts[$orderId])) {
+            $orderProducts[$orderId] = [];
+        }
+        $orderProducts[$orderId][] = $productId;
+//        print_r($orderProducts);
+
+    }
+    fclose($fileOrderProduct);
+} else {
+    echo 'не удалоцй';
+}
+
+
