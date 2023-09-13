4e542699c9aad31d4c7bc1ff776e1e906908d905
kutlumbek <Moralek228@gmail.com>
2023-09-13 23:09:21 +0300

    Теперь в бд добавляется информация о заказе

--- src/models/Auth/orders/order_handler.php
+++ src/models/Auth/orders/order_handler.php
@@ -1,20 +1,35 @@
 <?php
 session_start();
-$filePath = __DIR__ .'/../../../../storage_files/order_product.txt';
+$orderProductPath = __DIR__ .'/../../../../storage_files/order_product.txt';
+$orderPath = __DIR__ .'/../../../../storage_files/order.txt';
 
-$handlerOrder = fopen($filePath, 'a + b');
-$dataOrder = file($filePath, FILE_IGNORE_NEW_LINES);
+$handlerOrderProduct = fopen($orderProductPath, 'a + b');
+$handlerOrder = fopen($orderPath, 'a + b');
+
+$dataOrderProduct = file($orderProductPath, FILE_IGNORE_NEW_LINES);
+$dataOrder = file($orderPath, FILE_IGNORE_NEW_LINES);
 
 $orderData = $_SESSION['cart_item']; // Данные о товарах из сессии
 $userId = $_SESSION['user']['id'];
 
-$orderId = $dataOrder ? (intval(explode('|', end($dataOrder))[0]) + 1) : 1;
-//order|user|product|count
+$orderProductId = $dataOrderProduct ? (intval(explode('|', end($dataOrderProduct))[0]) + 1) : 1;
+$orderId = $dataOrder ? (intval(explode('|', end($dataOrder))[1]) + 1) : 1;
+
+//order_product:
+//orderId|userId|productId|count
+
+//order:
+// idUser|idOrder|
 
 foreach ($orderData as $item) {
-    $formattedData = "{$orderId}|{$userId}|{$item['code']}|{$item['quantity']}";
-
-    fwrite($handlerOrder, $formattedData . PHP_EOL);
-
+    $formattedOrderProductData = "{$orderId}|{$userId}|{$item['code']}|{$item['quantity']}";
+    fwrite($handlerOrderProduct, $formattedOrderProductData . PHP_EOL);
 }
+
+$formattedOrderData = "{$userId}|{$orderId}";
+fwrite($handlerOrder, $formattedOrderData . PHP_EOL);
+
+fclose($handlerOrder);
+fclose($handlerOrderProduct);
+
 
